3e5b9f16926dad2dc70f409cbc10e9e9c635eb34
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2021-10-22 10:24:21 +0530

    CommandLineArgs.java

--- CommandLineArgs.java
+++ CommandLineArgs.java
@@ -1,7 +1,25 @@
+import java.lang.*;
+import java.io.*;
 public class CommandLineArgs {
-	public static void main(String[] args) {
-		for (String str:args) {
-			System.out.println(str);
+	public static void main(String[] args) {	
+		String nameArray[]= new String[11];
+		nameArray[0]= "First";
+		nameArray[1]= "Second";
+		nameArray[2]= "Third";
+		nameArray[3]= "Forth";
+		nameArray[4]= "Fifth";
+		nameArray[5]= "Sixth";
+		nameArray[6]= "Seventh";
+		nameArray[7]= "Eighth";
+		nameArray[8]= "Ninth";
+		nameArray[9]= "Tenth";
+		nameArray[10]= "Eleventh";
+		int i = 0;
+		for(String str : args)
+		{	
+			System.out.println(nameArray[i]+" Student Name is = "+args[i]);	
+			i++;	
 		}
+		
 	}
 }
