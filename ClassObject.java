9746564b20339a79ddc6881c98f84306ea62ccca
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2021-11-18 09:46:48 +0530

    Update ClassObject.java

--- ClassObject.java
+++ ClassObject.java
@@ -1,75 +1,40 @@
 import java.io.*;
-import java.util.*;
 
 class Student
 {
-	private int enrNo[] = new int[10];
-	private int mark[]  = new int[10];
-	private int a;
-	private String name[] = new String[10];
-	private String gender[] = new String[10];
-	public Student()
-	{
+	private String name,eno,gender;
+	private int marks1,marks2;
+	private static int count=0;
+	Student(){this("","","",0,0);
+
 	}
-	public Student(int enr,int mk,String gen,String nm,int a)
+	Student(String eno,String name,String gender,int marks1,int marks2)
 	{
-		
-		enrNo[a] = enr;
-		name[a] = nm;
-		gender[a] = gen;
-		mark[a] = mk;
+		this.eno=eno;
+		this.name=name;
+		this.gender=gender;
+		this.marks1=marks1;
+		this.marks2=marks2;
+		count++;
 	}
-	public void display(int a)
+
+	public void display()
 	{
-		System.out.print("\n\tTotal Numbers of Object is  "+a);			
-		for(int i = 0;i<a;i++)
-		{
-			System.out.print("\n\tEnroll No. : "+enrNo[i]);
-			System.out.print("\n\tName : "+name[i]);
-			System.out.print("\n\t Gender : "+gender[i]);
-			System.out.print("\n\t Marks : "+mark[i]);			
-		}
-		
+		System.out.print("\n\n\tEnrollment no. :"+eno);
+		System.out.print("\n\tName           :"+name);
+		System.out.print("\n\tGender         :"+gender);
+		System.out.print("\n\tSubject 1      :  "+marks1);
+		System.out.print("\n\tSubject 2      :  "+marks2);
+		System.out.print("\n\tTotal Students :"+count+"\n\n");
 	}
 }
-public class ClassObject
+public class Q6
 {
-	public static void main(String args[])
+	public static void main(String a[])
 	{
-		int  a=0,ch,mark,enr;
-		String name,gender;
-		Student st[] = new Student[10];
-		do{
-			System.out.print("\n\t1. For New Record");
-			System.out.print("\n\t2. For Display All Records");
-			System.out.print("\n\t0. For Exit");
-			System.out.print("\n\tSelect your Choice");
-			Scanner sc = new Scanner(System.in);
-			ch = Integer.parseInt(sc.nextLine());
-			switch(ch)
-			{
-				case 1:
-						
-						System.out.print("\n\tEnter Enrollment Number : ");
-						enr = Integer.parseInt(sc.nextLine());
-						System.out.print("\n\tEnter Student Name");
-						name = sc.nextLine();
-						System.out.print("\n\tEnter Gender : ");
-						gender = sc.nextLine();
-						System.out.print("\n\tEnter The Marks : ");
-						mark = Integer.parseInt(sc.nextLine());	
-					 	st[a] = new Student(enr,mark,gender,name,a);
-						a++;
-					break;
-				case 2:
-					Student stt = new Student(); 					
-					stt.display(a);
-					break;
-				case 0:
-					return;
-				default:
-					System.out.println("Wrong Input");					
-			}
-		}while(ch!=0);	
-	}	
+		Student s= new Student();
+		s.display();
+		Student s2= new Student("SCET0021","Raj Patel;","Male",80,90);
+		s2.display();
+	}
 }
